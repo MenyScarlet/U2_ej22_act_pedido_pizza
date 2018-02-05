9c590598e7922cfcd4129fd89ae49436151cb969
MenyScarlet <melania.gallego.22@gmail.com>
2018-02-05 16:37:30 +0100

    Ejercicio pedido pizza

---
+++ app/src/main/java/com/example/scarlet/u2_ej22_act_pedido_pizza/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.scarlet.u2_ej22_act_pedido_pizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
